e6529311496b6cec8f1ab2ca4b3e796e2892b540
vcrazy@abv.bg <vcrazy@abv.bg>
2013-04-15 21:27:57 +0300

    test

---
+++ test.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<body>
+	nice.
+	<br />
+	<iframe src="iframe-parent.php" width="1000" height="600"></iframe>
+	<script>
+//		function check(){
+//			var hash = window.location.hash;
+//
+//			if(hash && hash.substr(1) == 'on'){
+//				window.location.hash = '';
+//			}
+//		}
+//
+//		setInterval(check, 1000);
+	</script>
+</body>
+</html>
